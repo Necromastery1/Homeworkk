eaa26d580526eec72eee0042ea695c465cc88e4c
SEMYON D <4610samara@gmail.com>
2022-09-03 14:49:15 +0300

    Arr func

--- Array/main.cpp
+++ Array/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 using namespace std;
 
-#define TASK_2
-//#define TASK_3
-
+//#define TASK_2
+#define TASK_3
+//#define TASK_4
+//#define TASK_5
+ 
 void main() 
 {
+//TASK_1 - https://github.com/Necromastery1/Hello, git@github.com:Necromastery1/Elochka.git, git@github.com:Necromastery1/dataTypes.git.
 #ifdef TASK_2
 	setlocale(LC_ALL, " ");
 	int summa = 0;
@@ -52,6 +55,57 @@ void main()
 	cout << "Минимальное значение = " << min_elem << ", его индекс = " << index_min << "\n" << endl;
 #endif
 #ifdef TASK_3
+
 	setlocale(LC_ALL, " ");
+	const int n = 10;
+	int arra[n] = {1,2,3,4,5,6,7,8,9,10};  
+	int t;
+	//4, 5, 6, 7, 8, 9, 10, 1, 2, 3
+	
+	cout << "\n\n\t Исходный массив:\n ";
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "\t" << arra[i];
+	}
+
+	cout << "\n\n\t Массив, сдвинутый на 3 элемента влево:\n ";
+	t = arra[0];
+	for (int i = 0; i < n; i++)
+	{
+		arra[i] = arra[i + 3];
+	}
+	arra[n - 3] = t;
+	for (int i = 0; i < n; i++)
+	{
+		cout << "\t" << arra[i];
+	}
+	cout << endl;
+
 #endif
+#ifdef TASK_4
+	int dec, bin = 0, j;
+
+	cin >> dec;
+	for (j = 0; dec > 0; j++)
+	{
+		bin += (dec % 2) * pow(10.0, j);
+		dec /= 2;
+	}
+
+	cout << bin << endl;
+#endif
+#ifdef TASK_5
+	int dec, bin = 0, j;
+
+	cin >> dec;
+	for (j = 0; dec > 0; j++)
+	{
+		bin += (dec % 2) * pow(16.0, j);
+		dec /= 2;
+	}
+
+	cout << bin << endl;
+#endif
+   
 }
